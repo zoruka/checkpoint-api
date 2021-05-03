b17ff49a855f9526899a5e4251cce51b373b7a43
Felipe Mendes <zo.fmendes@gmail.com>
2021-05-02 21:44:58 -0300

    :sparkles: add AddAccountRepository implementation

--- src/infra/database/mongodb/repositories/mongo-account-repository.ts
+++ src/infra/database/mongodb/repositories/mongo-account-repository.ts
@@ -1,1 +1,24 @@
-export class MongoAccountRepository {}
+import { AddAccountRepository } from '../../../../data/protocols';
+import { Account } from '../../../../domain/models';
+import { MongoHelper } from '../mongo-helper';
+
+export class MongoAccountRepository implements AddAccountRepository {
+	async add(
+		params: AddAccountRepository.Params
+	): Promise<AddAccountRepository.Result> {
+		const accountCollection = await MongoHelper.getCollection('account');
+
+		const newAccount: Omit<Account.Model, 'id'> = {
+			...params,
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		};
+
+		const result = await accountCollection.insertOne(newAccount);
+
+		return {
+			...newAccount,
+			id: result.insertedId,
+		};
+	}
+}
