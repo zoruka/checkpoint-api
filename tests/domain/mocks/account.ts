19a87b152b001e335441c988cde72f6684f0a32f
Felipe Mendes <zo.fmendes@gmail.com>
2021-05-03 08:58:57 -0300

    :white_check_mark: add mockAddAccountParams

--- tests/domain/mocks/account.ts
+++ tests/domain/mocks/account.ts
@@ -1,5 +1,6 @@
 import { datatype, system, internet, name, random } from 'faker';
 import { Account } from '../../../src/domain/models';
+import { AddAccount } from '../../../src/domain/usecases';
 
 export const mockAccount = (
 	params: Partial<Account.Model> = {}
@@ -12,5 +13,16 @@ export const mockAccount = (
 	name: params.name || name.findName(),
 	password: params.password || random.alphaNumeric(32),
 	updatedAt: params.updatedAt || new Date(),
+	username: params.username || name.firstName(),
+});
+
+export const mockAddAccountParams = (
+	params: Partial<AddAccount.Params> = {}
+): AddAccount.Params => ({
+	access: params.access || Account.Access.Profile,
+	avatarPath: params.avatarPath || system.filePath(),
+	email: params.email || internet.email(),
+	name: params.name || name.findName(),
+	password: params.password || random.alphaNumeric(32),
 	username: params.username || name.firstName(),
 });
