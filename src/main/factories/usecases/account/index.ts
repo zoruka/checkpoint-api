d67177227b2505c3f2c152f86854eba397f01070
Felipe Mendes <zo.fmendes@gmail.com>
2021-06-13 02:18:40 -0300

    :sparkles: add makeFindAccountValidation

--- src/main/factories/usecases/account/index.ts
+++ src/main/factories/usecases/account/index.ts
@@ -1,2 +1,3 @@
 export * from './add-account-factory';
 export * from './auth-factory';
+export * from './find-account-factory';
---
+++ src/main/factories/validaton/find-account-validation-factory.ts
@@ -0,0 +1,14 @@
+import { Validation } from '../../../presentation/protocols';
+import {
+	CompositeValidation,
+	RequiredValidation,
+} from '../../../validation/validations';
+import { makeRequiredValidator } from '../infras';
+
+export const makeFindAccountValidation = (): Validation => {
+	const requiredValidator = makeRequiredValidator();
+
+	return new CompositeValidation([
+		new RequiredValidation(requiredValidator, 'id'),
+	]);
+};
