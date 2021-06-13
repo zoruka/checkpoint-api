63caaf475d10273c99be4f49dcd62ad5612f0768
Felipe Mendes <zo.fmendes@gmail.com>
2021-06-13 01:41:56 -0300

    :bug: fix usename and password regex

--- src/infra/validator/string-validator-adapter.ts
+++ src/infra/validator/string-validator-adapter.ts
@@ -16,13 +16,13 @@ export class UsernameValidatorAdapter implements UsernameValidator {
 	async validateUsername(username: string): Promise<void | string> {
 		if (username.length < 3) return 'minLength=3';
 		if (username.length > 12) return 'maxLength=12';
-		if (!/[a-zA-Z0-9_]{3,12}/.test(username)) return 'regex';
+		if (!/[a-zA-Z0-9_]{3,12}$/.test(username)) return 'regex';
 	}
 }
 
 export class PasswordValidatorAdapter implements PasswordValidator {
 	async validatePassword(password: string): Promise<void | string> {
 		if (password.length < 8) return 'minLength=8';
-		if (!/[a-zA-Z0-9_!@#$%^&*-]{8,}/.test(password)) return 'regex';
+		if (!/[a-zA-Z0-9_!@#$%^&*()-]{8,}$/.test(password)) return 'regex';
 	}
 }
