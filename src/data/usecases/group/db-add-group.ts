a587c19ccaeacfbf612001e58e361701e4c39470
Felipe Mendes <zo.fmendes@gmail.com>
2021-06-13 16:08:12 -0300

    :sparkles: add admin id validation

--- src/data/usecases/group/db-add-group.ts
+++ src/data/usecases/group/db-add-group.ts
@@ -1,7 +1,9 @@
 import { AddGroup } from '../../../domain/usecases';
+import { DatabaseError } from '../../errors';
 import {
 	AddGroupBindingRepository,
 	AddGroupRepository,
+	FindAccountRepository,
 	GenerateGroupTagRepository,
 } from '../../protocols';
 
@@ -9,10 +11,18 @@ export class DbAddGroup implements AddGroup {
 	constructor(
 		private readonly generateGroupTagRepository: GenerateGroupTagRepository,
 		private readonly addGroupRepository: AddGroupRepository,
-		private readonly addGroupBindingRepository: AddGroupBindingRepository
+		private readonly addGroupBindingRepository: AddGroupBindingRepository,
+		private readonly findAccountRepository: FindAccountRepository
 	) {}
 
 	async add(params: AddGroup.Params): Promise<AddGroup.Result> {
+		const adminAccount = await this.findAccountRepository.findOne(
+			params.adminId
+		);
+
+		if (!adminAccount)
+			throw new DatabaseError.NotFound('User id not found');
+
 		const tag = await this.generateGroupTagRepository.generateTag();
 		const date = new Date();
 
