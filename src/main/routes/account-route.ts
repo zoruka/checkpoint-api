80fc2817ec0409d4c56ee54a8b06437795c0cdca
Felipe Mendes <zo.fmendes@gmail.com>
2021-06-12 22:16:45 -0300

    :sparkles: add controller factory

--- src/main/routes/account-route.ts
+++ src/main/routes/account-route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { adaptRoute } from '../adapters';
+import { makeRegisterAccountController } from '../factories/controllers';
 
 export const applyAccountRoutes = (router: Router): void => {
-	router.post('/register', adaptRoute('' as any));
+	router.post('/register', adaptRoute(makeRegisterAccountController()));
 };
