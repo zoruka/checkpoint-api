4b6f55f4d9e7b200c45a310d65bcc6e01d88f3d7
Felipe Mendes <zo.fmendes@gmail.com>
2021-06-13 04:15:35 -0300

    :sparkles: add jwtKey env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
 	mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/checkpoint',
 	port: process.env.SERVER_POT || '4003',
+	jwtKey: process.env.JWT_KEY || 'secret',
 };
--- src/main/factories/infras/crypto-factory.ts
+++ src/main/factories/infras/crypto-factory.ts
@@ -1,9 +1,10 @@
 import { BcryptAdapter, JwtAdapter } from '../../../infra/crypto';
+import env from '../../config/env';
 
 export const makeBcryptAdapter = (): BcryptAdapter => {
 	return new BcryptAdapter(8);
 };
 
 export const makeJwtAdapter = (): JwtAdapter => {
-	return new JwtAdapter('secret');
+	return new JwtAdapter(env.jwtKey);
 };
