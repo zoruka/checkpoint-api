c913d797d84a7b3c06e425a82c2893debd8aeafb
Felipe Mendes <zo.fmendes@gmail.com>
2021-06-13 18:27:33 -0300

    :sparkles: add exports

--- src/data/usecases/group/index.ts
+++ src/data/usecases/group/index.ts
@@ -3,3 +3,4 @@ export * from './db-find-group-by-id';
 export * from './db-find-groups';
 export * from './db-bind-group-account';
 export * from './db-update-group';
+export * from './db-find-group-accounts';
