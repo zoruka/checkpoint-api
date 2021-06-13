7cf4882a4c99f68f4ca472b60208ada831527fac
Felipe Mendes <zo.fmendes@gmail.com>
2021-06-13 00:33:05 -0300

    :art: rename variable

--- src/validation/validations/required-validation.ts
+++ src/validation/validations/required-validation.ts
@@ -3,12 +3,12 @@ import { RequiredValidator } from '../protocols';
 
 export class RequiredValidation implements Validation {
 	constructor(
-		private readonly emailValidator: RequiredValidator,
+		private readonly validator: RequiredValidator,
 		private readonly objectKey: string
 	) {}
 
 	async validate(input: any): Validation.Result {
-		const error = await this.emailValidator.validateRequired(
+		const error = await this.validator.validateRequired(
 			input[this.objectKey]
 		);
 		if (error) return { [this.objectKey]: error };
