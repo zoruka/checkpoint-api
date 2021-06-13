4950135d36c56d4e0483c69e39e6a5420e640857
Felipe Mendes <zo.fmendes@gmail.com>
2021-06-13 00:51:16 -0300

    :sparkles: add makeRequiredValidator

--- src/main/factories/infras/validator-factory.ts
+++ src/main/factories/infras/validator-factory.ts
@@ -1,5 +1,12 @@
-import { StringValidatorAdapter } from '../../../infra/validator';
+import {
+	RequiredValidatorAdapter,
+	StringValidatorAdapter,
+} from '../../../infra/validator';
 
 export const makeStringValidator = (): StringValidatorAdapter => {
 	return new StringValidatorAdapter();
+};
+
+export const makeRequiredValidator = (): RequiredValidatorAdapter => {
+	return new RequiredValidatorAdapter();
 };
