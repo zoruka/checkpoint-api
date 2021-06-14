4f2587514a470bb874376cb0d56cd063cb6f9d96
Felipe Mendes <zo.fmendes@gmail.com>
2021-06-14 01:24:02 -0300

    :sparkles: add FindGroupsController

---
+++ src/presentation/controllers/group/find-groups-controller.ts
@@ -0,0 +1,39 @@
+import { IdentifiedError } from '../../../domain/errors';
+import { Group } from '../../../domain/models';
+import { FindGroups } from '../../../domain/usecases';
+import { HttpError } from '../../errors';
+import { ok } from '../../helpers';
+import { Http, Validation } from '../../protocols';
+
+export class FindGroupsController implements Http.Controller {
+	constructor(
+		private readonly validation: Validation<FindGroupsController.Request>,
+		private readonly findGroups: FindGroups
+	) {}
+
+	async handle(
+		request: FindGroupsController.Request
+	): Promise<Http.Response<FindGroupsController.Response>> {
+		try {
+			const badParams = await this.validation.validate(request);
+			if (badParams) return new HttpError.BadRequest(badParams);
+
+			const group = await this.findGroups.find(request.search);
+
+			return ok(group);
+		} catch (e) {
+			if (e instanceof IdentifiedError) {
+				return new HttpError.Forbidden().pass(e);
+			} else {
+				return new HttpError.Server();
+			}
+		}
+	}
+}
+
+export namespace FindGroupsController {
+	export type Request = {
+		search: string;
+	};
+	export type Response = Group.Model[];
+}
--- src/presentation/controllers/group/index.ts
+++ src/presentation/controllers/group/index.ts
@@ -2,3 +2,4 @@ export * from './register-group-controller';
 export * from './find-group-accounts-controller';
 export * from './find-group-by-id-controller';
 export * from './bind-group-account-controller';
+export * from './find-groups-controller';
